6340ebbe675ccf1ef84d0c180ea1ff4eced5eed2
Nick Bellamy <nick.bellamy@gmail.com>
2018-01-19 11:02:51 +0000

    Change querySelectorAll to querySelector
    Additioanlly, fix the data- parameters in the selector so they correctly pick the desired input.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 
 // Remove value from candidates at location (row/col)
 function removeCandidate(row, col, value) {
-    // Select location depending on row and col ([0] needed as querySelectorAll retrurns an array)
-    let location = document.querySelectorAll('[data-row="' + row + '"], [data-col="' + col + '"]')[0];
+    // Select location depending on row and col
+    let location = document.querySelector('[data-row="' + row + '"][data-col="' + col + '"]');
     // Parse JSON
     let candidates = JSON.parse(location.dataset.candidates);
     // Remove value from candidates array
@@ -10,10 +10,10 @@ function removeCandidate(row, col, value) {
     // Convert back into JSON object
     let result = JSON.stringify(candidates);
     // Update data-candidates attribute at location
-    document.querySelectorAll('[data-row="' + row + '"], [data-col="' + col + '"]')[0].dataset.candidates = result;
+    document.querySelector('[data-row="' + row + '"][data-col="' + col + '"]').dataset.candidates = result;
     
     // Debug log
-    console.log(document.querySelectorAll('[data-row="' + row + '"], [data-col="' + col + '"]')[0]);
+    console.log(document.querySelector('[data-row="' + row + '"][data-col="' + col + '"]'));
 }
 
 // Event listeners for buttons
